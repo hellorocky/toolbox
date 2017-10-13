6029efedc79a00cdd065dbb769d46f8758e5b01b
RockyWu <hellorocky728@gmail.com>
2017-10-13 15:13:39 +0800

    first init

---
+++ macOS/myfile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+文件上传/下载脚本(到自己的VPS中)
+
+使用方法:
+    将该文件放到PATH中,并把如下信息加入zshrc:
+    alias up='python myfile.py up'
+    alias down='python myfile.py down'
+
+    up filename即可
+"""
+import sys
+import subprocess
+
+UPLOAD_URL = "https://rockywu.me/up/"
+DOWNLOAD_URL = "https://rockywu.me/down/"
+
+def run_cmd(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    p.wait()
+    out = p.stdout.readlines()
+    return p.returncode, out
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit(1)
+    if sys.argv[1] in ("u", "up"):
+        cmd = "curl --insecure -T {filename} {url}".format(filename=sys.argv[2], url=UPLOAD_URL)
+    elif sys.argv[1] in ("d", "down"):
+        cmd = "wget  --no-check-certificate {url}{filename}".format(filename=sys.argv[2], url=UPLOAD_URL)
+    print("**" * 10 + DOWNLOAD_URL+sys.argv[2] + "**" * 10)
+    run_cmd(cmd)
