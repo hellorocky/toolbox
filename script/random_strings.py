7cdcfc7dd7cdf09520966b454f0a6c8cc7bf9fc7
RockyWu <hellorocky728@gmail.com>
2019-01-18 10:47:15 +0800

    bugfix

--- script/random_strings.py
+++ script/random_strings.py
@@ -1,8 +1,13 @@
 import string
 import random
 
-def random_string(length):
-    return "".join(random.choices(string.ascii_letters+string.digits, k=length))
+def random_string(length, salt=False):
+    salt = "!@#$%^&*()><?"
+    if salt:
+        return "".join(random.choices(string.ascii_letters+string.digits, k=length-1)) + random.choice(salt)
+    else:
+        return "".join(random.choices(string.ascii_letters+string.digits, k=length))
+
 
 if __name__ == "__main__":
-    print(random_string(16))
+    print(random_string(15, True))
